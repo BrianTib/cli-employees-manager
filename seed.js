099ddfcfd5816ecfc5f80498db36ee453826043c
Brian T. <bptiburcio@gmail.com>
2024-07-03 20:45:03 -0400

    Adds seeding to database init using faker. Renames init.js to seed.js to avoid npm init confusion

---
+++ seed.js
@@ -0,0 +1,145 @@
+/**
+ * This init file initializes the database
+ * and seeds it.
+ * 
+ * The user can then run npm start to interact
+ * with the CLI that controls the database
+*/
+import { createPostgresClient } from "./util/postgres.js";
+import { faker } from '@faker-js/faker';
+
+async function resetDatabase() {
+    const client = await createPostgresClient("postgres");
+    
+    // Drop and recreate the database
+    try {
+        await client.query("DROP DATABASE IF EXISTS employees_db");
+        await client.query("CREATE DATABASE employees_db");
+        console.log("Database reset successful.");
+    } catch (error) {
+        console.error("Error resetting database:", error);
+    } finally {
+        await client.end();
+    }
+}
+
+async function createTables() {
+    const client = await createPostgresClient("employees_db");
+
+    try {
+        const queries = [
+            // Add the departments table
+            `
+            CREATE TABLE IF NOT EXISTS department (
+                id SERIAL PRIMARY KEY,
+                name VARCHAR(64) NOT NULL
+            )
+            `,
+            // Add the roles table
+            `
+            CREATE TABLE IF NOT EXISTS role (
+                id SERIAL PRIMARY KEY,
+                title VARCHAR(64) UNIQUE NOT NULL,
+                salary DECIMAL NOT NULL,
+                department_id INTEGER NOT NULL,
+                FOREIGN KEY (department_id) REFERENCES department(id)
+            )
+            `,
+            // Add the employees table
+            `
+            CREATE TABLE IF NOT EXISTS employee (
+                id SERIAL PRIMARY KEY,
+                first_name VARCHAR(255) NOT NULL,
+                last_name VARCHAR(255) NOT NULL,
+                role_id INTEGER NOT NULL,
+                manager_id INTEGER,
+                FOREIGN KEY (role_id) REFERENCES role(id),
+                FOREIGN KEY (manager_id) REFERENCES employee(id)
+            )
+            `
+        ];
+
+        // Execute all of the queries
+        for (const query of queries) {
+            await client.query(query);
+        }
+
+        console.log("Tables created successfully for employee_db.");
+    } catch (error) {
+        console.error("Error creating tables:", error);
+    } finally {
+        await client.end();
+    }
+}
+
+async function seedTables() {
+    const client = await createPostgresClient("employees_db");
+
+    try {
+        // Create an arbitrary number of departments
+        const departments = [];
+        const departmentsCount = 10;
+        for (let i = 0; i < departmentsCount; i++) {
+            const departmentName = faker.commerce.department();
+            if (departments.includes(departmentName)) {
+                // Skip this iteration
+                i--;
+                continue;
+            }
+            // Insert into department table a new department
+            // and get back it's ID
+            await client.query(
+                'INSERT INTO department (name) VALUES ($1)',
+                [departmentName]
+            );
+        }
+
+        // Create an arbitrary number of roles
+        const titles = [];
+        const rolesCount = 15;
+        for (let i = 0; i < rolesCount; i++) {
+            const title = faker.person.jobTitle();
+            if (titles.includes(title)) {
+                // Skip this iteration
+                i--;
+                continue;
+            }
+
+            const salary = faker.finance.amount({ min: 30000, max: 150000, dec: 0 });
+            const departmentId = Math.floor(Math.random() * departmentsCount) + 1;
+            await client.query(
+                'INSERT INTO role (title, salary, department_id) VALUES ($1, $2, $3)',
+                [title, salary, departmentId]
+            );
+        }
+
+        // Generate the employees
+        for (let i = 0; i < 100; i++) {
+            const firstName = faker.person.firstName();
+            const lastName = faker.person.lastName();
+            const roleId = Math.floor(Math.random() * rolesCount) + 1;
+            // The first generated employees are managers as long
+            // as they are less than the number of departments
+            const isManager = i < departmentsCount;
+
+            await client.query(
+                'INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ($1, $2, $3, $4)',
+                [firstName, lastName, roleId, isManager ? i + 1 : null]
+            );
+        }
+
+        console.log('Database seeded successfully');
+    } catch (err) {
+        console.error('Error seeding database:', err);
+    } finally {
+        await client.end();
+    }
+}
+
+async function init() {
+    await resetDatabase();
+    await createTables();
+    await seedTables();
+}
+
+init();
--- util/postgres.js
+++ util/postgres.js
@@ -1,5 +1,4 @@
 import pg from 'pg';
-import { faker } from '@faker-js/faker';
 const { Client } = pg;
 
 // Return a new postgress client
@@ -15,52 +14,4 @@ export async function createPostgresClient(database = "employees_db") {
     //await generateMockData(client);
 
     return client;
-}
-
-// Generate mock data and insert into the database
-async function generateMockData(client) {
-    try {
-        // Create a random amount of departments
-        const departmentsCount = 25;
-        for (let i = 0; i < departmentsCount; i++) {
-            const departmentName = faker.commerce.department();
-            // Insert into department table a new department
-            // and get back it's ID
-            await client.query(
-                'INSERT INTO department (name) VALUES ($1)',
-                [departmentName]
-            );
-        }
-
-        for (let i = 0; i < 100; i++) {
-            // Generate fake data
-            const firstName = faker.person.firstName();
-            const lastName = faker.person.lastName();
-            const title = faker.person.jobTitle();
-            const salary = faker.finance.amount({ min: 30000, max: 150000, dec: 2 });
-            const isManager = Math.random() <= 0.25;
-
-            const departmentId = Math.floor(Math.random() * departmentsCount) + 1;
-    
-            console.log({title, salary, departmentId});
-            // Insert into role table
-            const { rows: roleRows } = await client.query(
-                'INSERT INTO role (title, salary, department) VALUES ($1, $2, $3) RETURNING id',
-                [title, salary, departmentId]
-            );
-
-            console.log(roleRows);
-            const roleId = roleRows[0].id;
-    
-            // Insert into employee table
-            await client.query(
-                'INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ($1, $2, $3, $4)',
-                [firstName, lastName, roleId, isManager ? i + 1 : null]
-            );
-        }
-
-        console.log('Mock data generated successfully.');
-    } catch (err) {
-        console.error('Error generating mock data:', err);
-    }
 }
